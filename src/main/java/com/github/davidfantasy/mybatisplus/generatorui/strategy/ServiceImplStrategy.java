2961433c554d773a209dfed826d82f9171434346
andanyang <1218853253@qq.com>
2023-08-28 17:37:41 +0800

    模板修改

--- src/main/java/com/github/davidfantasy/mybatisplus/generatorui/strategy/ServiceImplStrategy.java
+++ src/main/java/com/github/davidfantasy/mybatisplus/generatorui/strategy/ServiceImplStrategy.java
@@ -1,6 +1,5 @@
 package com.github.davidfantasy.mybatisplus.generatorui.strategy;
 
-import com.baomidou.mybatisplus.generator.config.ConstVal;
 import lombok.Data;
 
 @Data
@@ -8,5 +7,6 @@ public class ServiceImplStrategy {
     /**
      * 自定义继承的ServiceImpl类全称，带包名
      */
-    private String superServiceImplClass = ConstVal.SUPER_SERVICE_IMPL_CLASS;
+    //private String superServiceImplClass = ConstVal.SUPER_SERVICE_IMPL_CLASS;
+    private String superServiceImplClass = "com.admin4j.framework.mp.service.impl.BizServiceImpl";
 }
--- src/main/java/com/github/davidfantasy/mybatisplus/generatorui/strategy/ServiceStrategy.java
+++ src/main/java/com/github/davidfantasy/mybatisplus/generatorui/strategy/ServiceStrategy.java
@@ -1,6 +1,5 @@
 package com.github.davidfantasy.mybatisplus.generatorui.strategy;
 
-import com.baomidou.mybatisplus.generator.config.ConstVal;
 import lombok.Data;
 
 @Data
@@ -9,7 +8,8 @@ public class ServiceStrategy {
     /**
      * 自定义继承的Service类全称，带包名
      */
-    private String superServiceClass = ConstVal.SUPER_SERVICE_CLASS;
+    //private String superServiceClass = ConstVal.SUPER_SERVICE_CLASS;
+    private String superServiceClass = "com.admin4j.framework.mp.service.IBizService";
 
-    
+
 }
